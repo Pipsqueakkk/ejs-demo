fa8ea017006dd9822edc286b53f0bb7034907d2a
Pipsqueakkk <92323897+Pipsqueakkk@users.noreply.github.com>
2022-11-04 01:29:05 +0200

    nodejs example control flow, escaped output

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // use res.render to load up an ejs view file
 
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wendesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 // index page
-app.get('/', function(req, res) {
-  console.log('Hello from nodeJS')
-  res.render('pages/index');
+app.get("/", function (req, res) {
+  console.log("Hello from nodeJS");
+  const message = "Hello node.js";
+
+  const date = new Date();
+
+  const dayOfWeek = date.getDay();
+
+  const today = daysOfWeek[dayOfWeek];
+  
+  res.render("pages/index", {
+    message: message,
+    dayOfWeek: dayOfWeek,
+    today: today,
+  });
 });
 
 // about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+app.get("/about", function (req, res) {
+  res.render("pages/about");
 });
 
 app.listen(8081);
-console.log('Server is listening on port 8081');
+console.log("Server is listening on port 8081");
